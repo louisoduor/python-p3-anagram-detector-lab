d6d68dfa72790bf8df82899239033061dbd797d2
louisoduor <louisodhiambo20@gmail.com>
2024-03-04 09:33:33 +0000

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+
+    def match(self,words):
+        """Returns a list of words that match the initialized word."""
+        return [word for word in words if self.sorted_word in ''.join(sorted(word)) == self.sorted_word]
+    
